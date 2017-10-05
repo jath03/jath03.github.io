e5ea62a4a2ef2438c50e1b34cadba0a0115e3a99
jath03 <jackthughes@yahoo.com>
2017-10-05 07:35:07 -0700

    Big changes

---
+++ src/static/js/index.jsx
@@ -0,0 +1,41 @@
+var $ = require("jquery");
+
+function openNav() {
+  if (screen.width < 500) {
+    document.getElementById("nav").style.width = "100%";
+  } else {
+    document.getElementById("nav").style.width = "25%";
+  }
+}
+
+function closeNav() {
+  document.getElementById("nav").style.width = "0";
+}
+
+
+
+function main() {
+  closeNav();
+  $("#header").click(function() {
+    $("#header").hide();
+    $("#header").fadeIn(1000);
+  });
+  $("#navButton").click(function () {
+    openNav();
+  });
+  $("#navButton").css("height", $("#header").css("height"));
+}
+
+
+$(document).ready(main);
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker.register("/sw.js").then(function(registration) {
+      console.log("registration was successful with scope: ", registration.scope);
+    }, function(err) {
+      alert("Service worker registration error: ", err);
+      console.log("Service worker registration error: ", err);
+    });
+  });
+}
